e7e50a09f80745a1214e6f672fc869e4ed3e0726
Luke McKinley <luke.mckinley21@gmail.com>
2020-03-25 11:33:02 -0500

    added the twitter app to the project, but it's empyt as of now

--- toilet_paper_project/toilet_paper_project/settings.py
+++ toilet_paper_project/toilet_paper_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stream_twitter',
+    'stream_django',
 ]
 
 MIDDLEWARE = [
---
+++ toilet_paper_project/twitterStream_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TwitterstreamAppConfig(AppConfig):
+    name = 'twitterStream_app'
