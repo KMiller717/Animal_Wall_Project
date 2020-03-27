900696d0320f2807126bb157e68981ad7f444606
Kmiller717 <millerk717@gmail.com>
2020-03-27 12:43:11 -0500

    final project

--- toilet_paper_project/toilet_paper_project/urls.py
+++ toilet_paper_project/toilet_paper_project/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('wall/', include('animal_app.urls'))
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
